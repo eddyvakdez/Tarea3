4dab4f7da6a7e3e7b9d3817d7236a61e4dec2ce5
eddyvakdez <eddymiguelmartinez402@gmail.com>
2025-10-02 18:01:47 -0400

    Insertando link del video de experiencia

--- src/views/Experiencia.vue
+++ src/views/Experiencia.vue
@@ -12,7 +12,7 @@
     <ion-content class="ion-padding">
       <h1>Explicando mi experiencia al realizar esta tarea</h1>
       <iframe width="100%" height="315"
-      src=""
+      src="https://www.youtube.com/watch?v=-SJMp7dduYI"
       title="Video experiencia desde Youtube"
       frameborder="0"
       allowfullscreen></iframe>
